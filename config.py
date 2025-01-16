19fc22e0d06045d062e0724e6b7c084f9c2bb1d7
der-johanni <ivangorbanwork@gmail.com>
2025-01-16 16:50:56 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import pathlib
+
+BASE_DIR = pathlib.Path().cwd()
+STORAGE_PATH = BASE_DIR / 'data'
+DB_PATH = STORAGE_PATH / 'db' / 'data.db'
+FILE_PATH = STORAGE_PATH / 'images'
+
+TOKEN = '7687318476:AAEchCa3OgFeC6xV0QzApoMIZ2P_6GPJNO8'
---
+++ keyboards/main.py
@@ -0,0 +1,48 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from db.crud import get_books
+from keyboards.config import ITEMS_PER_PAGE
+
+kb_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Add new book', callback_data='add_book')],
+    # [InlineKeyboardButton(text='Delete book', callback_data='delete_book')],
+    # [InlineKeyboardButton(text='Get book with ID', callback_data='get_book')],
+    [InlineKeyboardButton(text='Get book list', callback_data='navigate:1')]
+])
+
+kb_back = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Menu', callback_data='menu')]
+])
+
+async def kb_list(page: int = 1):
+    books = get_books()
+
+    total_items = books.get_size()
+    total_pages = (total_items + ITEMS_PER_PAGE - 1) // ITEMS_PER_PAGE
+
+    start = (page - 1) * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+    curr_books = books[start:end]
+
+    # Objects buttons
+    button_rows = []
+    for book in curr_books:
+        data = book.get_data()
+        button_rows.append([InlineKeyboardButton(text=data['name'], callback_data=f'get_file:{data["url"]}')])
+
+    keyboard = InlineKeyboardBuilder(button_rows)
+
+    # Navigation buttons
+    nav_buttons = []
+    if page > 1:
+        nav_buttons.append(InlineKeyboardButton(text='Back', callback_data=f'navigate:{page - 1}'))
+    if page < total_pages and total_items > ITEMS_PER_PAGE:
+        nav_buttons.append(InlineKeyboardButton(text='Next', callback_data=f'navigate:{page + 1}'))
+
+    if nav_buttons:
+        keyboard.row(*nav_buttons)
+
+    keyboard.add(InlineKeyboardButton(text='Menu', callback_data='menu'))
+
+    return keyboard.as_markup()
